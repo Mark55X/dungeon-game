39cb2895ff7eb92342dca82ae8585f7f25502a81
OllieTails <ollie.tails@gmail.com>
2018-06-24 20:30:16 +0200

    Added a text shown in the menu

--- src/main/com/onn/dungeongame/states/MenuState.java
+++ src/main/com/onn/dungeongame/states/MenuState.java
@@ -2,8 +2,20 @@ package com.onn.dungeongame.states;
 
 import java.awt.*;
 import com.onn.dungeongame.*;
+import com.onn.dungeongame.font.*;
 
 public class MenuState extends State {
+
+	private com.onn.dungeongame.font.Font font;
+
+	public MenuState() {
+		font = new com.onn.dungeongame.font.Font();
+		font.setWidth(61);
+		font.setHeight(65);
+		font.setCharCount(95);
+		font.loadFont("/font/font.png");
+		font.load();
+	}
 
 	@Override
 	public void tick() {
@@ -14,7 +26,7 @@ public class MenuState extends State {
 
 	@Override
 	public void render(Graphics g) {
-
+		font.write("Click anywhere to start ...", g, 0, Handler.getHeight() - 22, 23, 0, 22, 22);
 	}
 
 }
